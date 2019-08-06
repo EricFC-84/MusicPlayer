0e60b7b175fc9dee83dcf3d1c83af67b81be9cb7
EricFC-84 <francoeric@gmail.com>
2019-08-06 02:18:42 +0200

    Personalized player buttons

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { SongDataService } from '../services/song-data.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  SongDataService
+} from '../services/song-data.service';
+import {
+  stringify
+} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-player',
@@ -7,12 +15,73 @@ import { SongDataService } from '../services/song-data.service';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
-  back(){
-this._data.setView("song-list");
-  }
-  constructor(public _data:SongDataService) { }
+  audioElement: HTMLAudioElement = < HTMLAudioElement > document.querySelectorAll("audio")[0];
 
-  ngOnInit() {
+  audioLength: string = "00:00"
+
+  back() {
+    this._data.setView("song-list");
   }
+
+
+  play_pause(): void {
+
+    if (( < HTMLAudioElement > document.querySelectorAll("audio")[0]).paused == true) {
+      ( < HTMLAudioElement > document.querySelectorAll("audio")[0]).play();
+      ( < HTMLImageElement > document.querySelectorAll("#playBtn")[0]).setAttribute("src", "../../assets/player/pause.png");
+
+    } else {
+      ( < HTMLAudioElement > document.querySelectorAll("audio")[0]).pause();
+      ( < HTMLImageElement > document.querySelectorAll("#playBtn")[0]).setAttribute("src", "../../assets/player/play-button.png");
+    }
+    this.songLength();
+  }
+
+  stop(): void {
+    ( < HTMLAudioElement > document.querySelectorAll("audio")[0]).load();
+    ( < HTMLImageElement > document.querySelectorAll("#playBtn")[0]).setAttribute("src", "../../assets/player/play-button.png");
+  }
+
+  loop(): void {
+    if (( < HTMLAudioElement > document.querySelectorAll("audio")[0]).loop == true) {
+      ( < HTMLImageElement > document.querySelectorAll("#loopBtn")[0]).style.filter = "invert(0)";
+    } else {
+      ( < HTMLImageElement > document.querySelectorAll("#loopBtn")[0]).style.filter = "invert(1)";
+    }
+    ( < HTMLAudioElement > document.querySelectorAll("audio")[0]).loop = !( < HTMLAudioElement > document.querySelectorAll("audio")[0]).loop;
+
+  }
+  songLength(): void {
+
+    let time = this.formatTime(( < HTMLAudioElement > document.querySelectorAll("audio")[0]).duration);
+    // console.log((< HTMLAudioElement >   .duration);
+    this.audioLength = time;
+
+  }
+  formatTime(seconds: number): string {
+
+    let secondsStr: string = "";
+    let minutesStr: string = "";
+    let minutes = Math.floor(seconds / 60);
+
+    if (minutes < 10) {
+      minutesStr = "0" + minutes;
+    } else {
+      minutesStr = "" + minutes;
+    }
+
+    seconds = Math.floor(seconds % 60);
+    if (seconds < 10) {
+      secondsStr = "0" + seconds;
+    } else {
+      secondsStr = "" + seconds;
+    }
+
+    return minutesStr + ":" + secondsStr;
+  }
+
+  constructor(public _data: SongDataService) {}
+
+  ngOnInit() {}
 
 }
